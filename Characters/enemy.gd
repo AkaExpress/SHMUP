47a062271bbd3cf540f489061eb52045a5cc091b
rugmj <rugmj@rugmj.dev>
2023-04-03 22:36:28 +0000

    adds ai to enemy

--- Characters/enemy.gd
+++ Characters/enemy.gd
@@ -1,4 +1,20 @@
 extends CharacterBody2D
 
+@export var speed = 250
+
+func _physics_process(delta):
+	follow_player(delta)
+
+func follow_player(delta):
+	var target = get_parent().get_node("Player").position
+	look_at(target)
+	velocity += position.direction_to(target) * speed * delta
+	
+	velocity.x = clamp(velocity.x, -speed, speed)
+	velocity.y = clamp(velocity.y, -speed, speed)
+	
+	if position.distance_to(target) > 10:
+		move_and_slide()
+
 func die():
 	queue_free()
