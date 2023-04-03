270b206916ce54224c5a34d3733d2dfe8f98e34a
rugmj <rugmj@rugmj.dev>
2023-04-03 21:41:04 +0000

    adds enemy health system

---
+++ Characters/enemy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var maxHealth = 100
+
+@export var health = 100
+
+func hit(damage):
+	health = clampHealth(health - damage)
+
+func clampHealth(health):
+	if (health <= 0):
+		die()
+		return 0
+	if (health >= maxHealth):
+		return maxHealth
+	return health
+
+func die():
+	queue_free()
